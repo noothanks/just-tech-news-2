9c03f6b42668b6d208e939a4ecc8bd72c11710d7
noothanks <coreyfuller727@yahoo.com>
2022-07-13 14:27:05 -0400

    adding hashing

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 //destructure MODEL and DATATYPES from sequelize
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create user model and extend classes from Model object
 class User extends Model {}
@@ -42,8 +43,21 @@ User.init(
             }
         }
     },
+    
     //Table config
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            }
+          },
         //pass imported sequelize connection
         sequelize,
         //set suto-timestamp to false
