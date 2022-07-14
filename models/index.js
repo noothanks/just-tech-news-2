906def500386d0509dd1ae5a997847315e8bc422
noothanks <coreyfuller727@yahoo.com>
2022-07-14 15:37:39 -0400

    creating Post model routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
-module.exports = {User};
+//create associations
+//references id column in User model to link user_id in Post model
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//references
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = {User, Post};
